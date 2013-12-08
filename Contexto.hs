d80425e84f36f3371a49ac0727834d22198ebe7b
Melecio Ponte <pontezambrano@gmail.com>
2013-12-08 16:30:14 -0430

    Se implementan las funciones: 	*obtprim, obtseg y obtter 	*sumParFrec 	*elimDups, union 	*buscarFrec 	*constParFrec 	*sumCuadrados 	*calcDistancia

--- Contexto.hs
+++ Contexto.hs
@@ -33,7 +33,46 @@ agregarOrden1 (p, q, r) a = (p, q, agregar a r)
 agregar :: (Eq a) => a -> [(a, Frec)] -> [(a, Frec)]
 agregar x [] = [(x,1)]
 agregar x ((b, c):ys) =
-				if x == b then
-					(b, c+1):ys
-				else
-					(b,c): agregar x ys
+    if x == b then
+	(b, c+1):ys
+    else
+	(b,c): agregar x ys
+
+obtprim :: Contexto -> Frec
+obtprim (a,_,_) = a
+
+obtseg :: Contexto -> [(Evento, Frec)]
+obtseg (_,a,_) = a
+
+obtter :: Contexto -> [((Evento,Evento), Frec)]
+obtter (_,_,a) = a                 
+                 
+sumParFrec :: [(Frec,Frec)] -> Int
+sumParFrec [] = 0
+sumParFrec x = foldl (\a (b,c) -> a + (c - b)^2) 0 x
+
+elimDups :: (Eq a) => [a] -> [a]
+elimDups (x:xs) = if notElem x xs then
+                      x:elimDups xs
+                  else 
+                      elimDups xs
+elimDups [] = []
+
+union :: (Eq a) => [a] -> [a] -> [a]
+union xs ys = elimDups [x | x <- xs ]++[y | y <- ys, notElem y xs]
+
+buscarFrec :: (Eq a, Eq b, Num b) => a -> [(a, b)] -> b
+buscarFrec a (b:bs) = if (a == (fst b)) then
+                          snd b
+                      else buscarFrec a bs
+buscarFrec a [] = 0
+
+constParFrec :: (Eq a) => [a] -> [(a, Frec)] -> [(a, Frec)] -> [(Frec,Frec)]
+constParFrec (a:as) b c = ((buscarFrec a b),(buscarFrec a c)):constParFrec as b c
+constParFrec [] _ _ = []
+                      
+sumCuadrados :: (Eq a) => [(a, Frec)] -> [(a, Frec)] -> Int
+sumCuadrados a b = sumParFrec (constParFrec (union (map fst a) (map fst b))  a b)
+
+calcDistancia :: Contexto -> Contexto -> Float
+calcDistancia a b = sqrt  (fromIntegral ((obtprim a - obtprim b)^2 + sumCuadrados (obtseg a) (obtseg b) + sumCuadrados (obtter a) (obtter b)))
