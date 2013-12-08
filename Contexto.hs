c626cc298a0f1027ae079dd7bc3692d2b72d49e1
gabrielformica <gabrielformica93@gmail.com>
2013-12-08 16:26:00 -0430

    Se terminan las funciones necesarias para componer

--- Contexto.hs
+++ Contexto.hs
@@ -1,15 +1,18 @@
-import Data.List (elemIndex)
+import System.Random
+import Data.List
 
-type Evento = (Int, Int)
+type Evento = (Int, Int)  --Evento vacio (-1,-1)
 type Frec = Int --Frecuencia
 type Contexto = (Frec, [(Evento, Frec)], [((Evento, Evento), Frec)])
 
-type Prob = (Float, Float)
-type ListaProb = [Prob]
-
+type ProbsAcumulada = [Float]
 
 obtFrecVacia :: Contexto -> Frec
 obtFrecVacia (p, _, _) = p
+
+obtFrecPar :: (Evento, Evento) -> Contexto -> Frec
+obtFrecPar ((-1,-1), e) c = obtFrec e c
+obtFrecPar e (_, _, r) =  (map snd r) !! (obtIndice e (map fst r))
 
 obtFrec :: Evento -> Contexto -> Frec
 obtFrec e (_, q, _) = (map snd q) !! (obtIndice e (map fst q))
@@ -25,6 +28,9 @@ creaPares (x:y:zs) = (x,y): creaPares (y:zs)
 
 existeEven :: Evento -> Contexto -> Bool
 existeEven e (_, p, _) = e `elem` map fst p
+
+creaOrden1 :: Evento -> Evento -> (Evento, Evento)
+creaOrden1 a e = (a, e)
 
 agregarOrden0 :: Contexto -> Evento -> Contexto
 agregarOrden0 (0, [], []) a = (1, [(a, 1)], [])
@@ -49,7 +55,25 @@ obtIndice x y = obtIndice' x y 0
 		| x == head y = acc
 		| otherwise = obtIndice' x (tail y) (acc +1)
 
-calculaProbs :: Evento -> Contexto -> ListaProb --El evento vacio se considerara (-1,-1)
-obtProb :: Evento -> Evento -> Prob  --El primer evento vendria siendo el contexto (evento anterior)
+obtProb :: Contexto -> (Evento, Evento) -> Float
+obtProb c ((-1,-1), e)  = (fromIntegral (obtFrec e c))  / fromIntegral (obtFrecVacia c)
+obtProb c@(p, q, r) (a, e) =
+			 if (a, e) `elem` map fst r then
+				 0.3*((obtFrec e c) `divInt` obtFrecVacia c) + 0.7*((obtFrecPar (a, e) c) `divInt` obtFrec a c)
+			 else
+				0.0
+
+divInt ::  Int -> Int -> Float
+a `divInt` b = fromIntegral (a) / fromIntegral (b)
+
+
+obtListaProb :: Evento -> Contexto -> [Float]
+obtListaProb (-1, -1) (p, q, r) =  acumular $ (0.0:) $ map (`divInt` p) $ map snd q
+obtListaProb e c@(p, q, r) = acumular $ (0.0:) $  map (obtProb c) $ map (creaOrden1 e) (map fst q)
+
+acumular :: [Float] -> [Float]
+acumular [] = []
+acumular (x:[]) = []
+acumular (x:y:zs) = (x+y): acumular ((x+y):zs)
 
 
